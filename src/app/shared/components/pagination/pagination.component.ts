924f9fd36a89f01f02216846a4c80d961b992987
Danny Armijos <daniel2667@hotmail.es>
2025-04-17 16:37:10 +0200

    feat: mejorar componente de paginación y ajustar límite de productos en el servicio

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,15 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import {Component, computed, input, linkedSignal} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
 })
 export class PaginationComponent {
   pages = input(0);
   currentPage = input<number>(1);
-
+  activePage = linkedSignal(this.currentPage);
   getPagesList = computed(() => {
-    return Array.from({ length: this.pages() }, (_, i) => i + 1);
+    return Array.from({length: this.pages()}, (_, i) => i + 1);
   });
 }
