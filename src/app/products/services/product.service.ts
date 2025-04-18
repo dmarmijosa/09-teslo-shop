b76489e8a690ee779aed131d326400596d99aa9f
Danny Armijos <daniel2667@hotmail.es>
2025-04-18 12:38:33 +0200

    feat: mejorar lógica de caché de productos y corregir mensaje en la paginación

--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
-import { ProductResponse } from '@products/interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
-import { Product } from '../interfaces/product.interface';
+import {HttpClient} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {environment} from '@environments/environment';
+import {ProductResponse} from '@products/interfaces/product.interface';
+import {Observable, of, tap} from 'rxjs';
+import {Product} from '../interfaces/product.interface';
 
 const baseUrl = environment.baseUrl;
 interface Options {
@@ -18,15 +18,25 @@ interface Options {
 export class ProductService {
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductResponse>();
+
   getProducts(options: Options): Observable<ProductResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+
+    const key = `${limit}-${offset}-${gender}`;
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
     return this.http.get<ProductResponse>(`${baseUrl}/products`, {
       params: {
         limit,
         offset,
         gender,
       },
-    }).pipe(tap((resp)=> console.log(resp)));
+    }).pipe(
+      tap((resp) => console.log(resp)),
+      tap((resp)=> this.productsCache.set(key,resp))
+    );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
