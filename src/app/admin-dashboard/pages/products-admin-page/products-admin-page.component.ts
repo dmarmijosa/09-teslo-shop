0c0f128e6e15ca611e194ee70fdfdb2cafff1892
Danny Armijos <daniel2667@hotmail.es>
2025-04-25 14:28:31 +0200

    feat: mejorar la página de administración de productos con paginación y tabla de productos

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductTableComponent } from '@products/components/product-table/product-table.component';
+
+import { ProductService } from '@products/services/product.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+  productService = inject(ProductService);
+  paginationService = inject(PaginationService);
+  productPerPage = signal(10);
+
+  productResource = rxResource({
+    request: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productPerPage(),
+    }),
+    loader: ({ request }) => {
+      return this.productService.getProducts({
+        offset: request.page * request.limit,
+        limit: request.limit,
+      });
+    },
+  });
+}
